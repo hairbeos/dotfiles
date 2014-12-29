0ffb86c01093c7bb48c2d49bd398cf554aa55850
PATAHA <hairbeos@gmail.com>
2014-12-29 21:41:11 +0700

    update rvm install script

--- rvm/install.sh
+++ rvm/install.sh
@@ -4,9 +4,8 @@
 if test ! $(which rvm) 
 then
   info "Installing RVM with Ruby ..."
-  curl -#LO https://rvm.io/mpapis.asc
-  gpg --import mpapis.asc
-  ruby -e "$(curl -sSL https://raw.githubusercontent.com/wayneeseguin/rvm/master/binscripts/rvm-installer | bash -s stable --ruby)"
+  gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
+  ruby -e "$(curl -sSL https://raw.githubusercontent.com/wayneeseguin/rvm/master/binscripts/rvm-installer | bash -s stable --ruby --auto-dotfiles)"
 fi
 
 # reload rvm
